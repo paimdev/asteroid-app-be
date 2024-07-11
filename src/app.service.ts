710422f2b09580087a6f379f42ae456eca870f71
paimdev <lucas@paim.dev>
2024-07-11 17:09:25 +0100

    feat: setup services

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 import axios from 'axios';
 
 @Injectable()
 export class AppService {
+  private prisma = new PrismaClient();
   async serviceFetchAsteroids(): Promise<string> {
     const response = await axios.get(
-      `https://api.nasa.gov/neo/rest/v1/feed?api_key=Cpwplej7wHG1wtnO9ITZnKZD6qEmVZTBAH0imYfZ`,
+      `https://api.nasa.gov/neo/rest/v1/feed?api_key=${process.env.API_KEY}`,
     );
     return response.data.near_earth_objects;
   }
@@ -15,8 +17,22 @@ export class AppService {
     endDate: string,
   ): Promise<string> {
     const response = await axios.get(
-      `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=Cpwplej7wHG1wtnO9ITZnKZD6qEmVZTBAH0imYfZ`,
+      `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=${process.env.API_KEY}`,
     );
     return response.data.near_earth_objects;
+  }
+
+  async serviceAddFavoriteAsteroid(id: string, name: string, url: string) {
+    return this.prisma.favoriteAsteroid.create({
+      data: {
+        id,
+        name,
+        url,
+      },
+    });
+  }
+
+  async serviceFetchFavoriteAsteroids() {
+    return this.prisma.favoriteAsteroid.findMany();
   }
 }
